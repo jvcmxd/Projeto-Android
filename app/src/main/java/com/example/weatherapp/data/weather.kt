1be5b5dfe46fe2d076419b44c33e0535a1173dc7
João Victor Costa Machado <j.victorcosta27@dinfor.uniceub>
2024-06-13 08:24:12 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/weatherapp/data/weather.kt
@@ -0,0 +1,41 @@
+package com.example.weatherapp.data
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherapp.Greeting
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+
+data class weather: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Weather()
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Weather(){
+
+}
+
+@Preview
+@Composable
+fun Weather()
